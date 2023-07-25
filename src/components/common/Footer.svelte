0365cb9652c59833e0fb46d1ffa84ff721ca7adb
surajgoraya <SurajGoraya@outlook.com>
2023-07-25 15:07:12 -0400

    feat: :page_facing_up: add MAL license reference

--- src/components/common/Footer.svelte
+++ src/components/common/Footer.svelte
@@ -1,7 +1,16 @@
 
 <footer class="container">
     <hr />
-    <small>&copy; {new Date().getFullYear().toString()} · Born out of Beardgel's disappointment •
-        <a href="https://github.com/surajgoraya/aot-tracker">Source code</a></small
-    >
+    <small>&copy; {new Date().getFullYear().toString()} · Born out of Beardgel's disappointment · <a href="https://github.com/surajgoraya/aot-tracker">Source code</a>
+        <div class="smaller">
+            ◾️ Data courtesy of MyAnimeList.net, licensed under the <a href="https://myanimelist.net/static/apiagreement.html">API License and Developer Agreement</a>
+        </div>
+        </small>
 </footer>
+
+<style>
+    .smaller {
+        opacity: 0.75;
+        font-size: .75rem;
+    }
+</style>
